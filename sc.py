2f1f8a938f7312f9876f2b781b6e501089f1e8cc
jarnocampi98 <62843550+jarnocampi98@users.noreply.github.com>
2020-04-11 01:58:35 +0200

    Add files via upload

--- sc.py
+++ sc.py
@@ -1,13 +1,19 @@
 import tkinter as tk
+import os
 def accedi():
     win.destroy()
     import accedi.py
 def registrati():
     win.destroy()
-    import registrati.py
+    import r.py
 def esci():
     win.destroy()
-    
+def u():
+    dir=os.getcwd()
+    try:
+        os.mkdir(dir+'/u')
+    except:
+        print ("")
 win=tk.Tk()
 win.title("Satysfind chat")
 win.rowconfigure(0, weight=2, minsize=800)
@@ -19,4 +25,3 @@ b1=tk.Button(master=f1, text="ACCEDI",fg="green", command=accedi).pack()
 b2=tk.Button(master=f1, text="REGISTRATI", bg="blue",fg="orange",command=registrati).pack()
 b3=tk.Button(master=f1, text="ESCI",bg="blue",fg="red", command=esci).pack()
 win.mainloop()
-
